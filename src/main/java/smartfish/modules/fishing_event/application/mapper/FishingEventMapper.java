6fef7c9804c5e06dd95c555d95ee1a12f6923830
GustavoHPelissari <gustavo.h.pelissari@gmail.com>
2026-07-13 19:35:20 -0300

    feat: adiciona mapper para FishingEvent

---
+++ src/main/java/smartfish/modules/fishing_event/application/mapper/FishingEventMapper.java
@@ -0,0 +1,28 @@
+package smartfish.modules.fishing_event.application.mapper;
+
+import org.springframework.stereotype.Component;
+import smartfish.modules.fishing_event.application.dto.request.create.CreateFishingEventRequest;
+import smartfish.modules.fishing_event.application.dto.response.create.CreateFishingEventResponse;
+import smartfish.modules.fishing_event.domain.FishingEventEntity;
+
+@Component
+public class FishingEventMapper {
+
+    public FishingEventEntity toEntity(CreateFishingEventRequest request) {
+
+        return FishingEventEntity.builder()
+                .eventDate(request.eventDate())
+                .eventTime(request.eventTime())
+                .build();
+    }
+
+    public CreateFishingEventResponse toDomain(FishingEventEntity entity) {
+
+        return new CreateFishingEventResponse(
+                entity.getId(),
+                entity.getFishingStatus(),
+                entity.getEventDate(),
+                entity.getEventTime()
+        );
+    }
+}
