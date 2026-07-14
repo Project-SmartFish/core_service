0b66608967b5fd8c3e845409a1a1c960c6522438
GustavoHPelissari <gustavo.h.pelissari@gmail.com>
2026-07-14 17:55:09 -0300

    refactor: adiciona método toEntity para update request dto de FishingEvent

--- src/main/java/smartfish/modules/fishing_event/application/mapper/FishingEventMapper.java
+++ src/main/java/smartfish/modules/fishing_event/application/mapper/FishingEventMapper.java
@@ -3,13 +3,27 @@ package smartfish.modules.fishing_event.application.mapper;
 import org.springframework.stereotype.Component;
 import smartfish.modules.fish_spot.domain.FishSpotEntity;
 import smartfish.modules.fishing_event.application.dto.request.create.CreateFishingEventRequest;
+import smartfish.modules.fishing_event.application.dto.request.update.UpdateFishingEventRequest;
 import smartfish.modules.fishing_event.application.dto.response.create.CreateFishingEventResponse;
+import smartfish.modules.fishing_event.application.dto.response.update.UpdateFishingEventResponse;
 import smartfish.modules.fishing_event.domain.core.FishingEventEntity;
 
 @Component
 public class FishingEventMapper {
 
     public FishingEventEntity toEntity(CreateFishingEventRequest request) {
+        FishSpotEntity fishSpot = FishSpotEntity.builder()
+                .id(request.fishSpotId())
+                .build();
+
+        return FishingEventEntity.builder()
+                .fishSpot(fishSpot)
+                .eventDate(request.eventDate())
+                .eventTime(request.eventTime())
+                .build();
+    }
+
+    public FishingEventEntity toEntity(UpdateFishingEventRequest request) {
         FishSpotEntity fishSpot = FishSpotEntity.builder()
                 .id(request.fishSpotId())
                 .build();
