5fce3bb8c15f05bb4bfc6ef42692e9fe6a37d6a3
Adryel Felipe <adryxd003@gmail.com>
2026-07-16 07:46:18 -0300

    feat: adiciona filtro de JWT que valida integridade do token

---
+++ src/main/java/smartfish/modules/auth/infrastructure/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package smartfish.modules.auth.infrastructure.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+import smartfish.modules.auth.infrastructure.jwt.JwtTokenProvider;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+
+        if(hasBearerToken(token)) {
+            // proceder
+        }
+    }
+
+    public boolean hasBearerToken(String token) {
+        return token != null && !token.isBlank() && token.startsWith("Bearer ") && token.length() > 7;
+    }
+}
