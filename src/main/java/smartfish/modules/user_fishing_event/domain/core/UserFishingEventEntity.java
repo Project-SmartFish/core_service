e934e3d7a1e73f686797d5053a65ca692b8e7c28
GustavoHPelissari <gustavo.h.pelissari@gmail.com>
2026-07-16 15:22:15 -0300

    feat: (UserFishingEvent): adiciona entity de lirelacionamento entre FishingEvent e User.

---
+++ src/main/java/smartfish/modules/user_fishing_event/domain/core/UserFishingEventEntity.java
@@ -0,0 +1,38 @@
+package smartfish.modules.user_fishing_event.domain.core;
+
+import jakarta.persistence.*;
+import lombok.*;
+import smartfish.modules.fishing_event.domain.core.FishingEventEntity;
+import smartfish.modules.user.domain.entity.UserEntity;
+
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "user_fishing_event")
+public class UserFishingEventEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+            name = "fishing_event_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_user_fishing_event_fishing_event")
+    )
+    private FishingEventEntity fishingEvent;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+            name = "user_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_user_fishing_event_user")
+    )
+    private UserEntity user;
+}
