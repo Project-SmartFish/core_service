bd5c407a2567d2634e46e7f5efc0a3bb03e48e9b
GustavoHPelissari <gustavo.h.pelissari@gmail.com>
2026-07-16 18:47:44 -0300

    feat(UserFishingEvent): adiciona camada de persistência para UserFishingEvent
    - Adiciona interface UserFishingEventRepository como abstração da camada de
      domínio
    - Adiciona interface IUserFishingEventJpaRepository para operações de
      persistência com Spring Data JPA
    - Adiciona repositório UserFishingRepositoryImpl implementando a
      integração entre interface de domínio e interface de JPA

---
+++ src/main/java/smartfish/modules/user_fishing_event/domain/repository/UserFishingEventRepository.java
@@ -0,0 +1,26 @@
+package smartfish.modules.user_fishing_event.domain.repository;
+
+import smartfish.modules.user_fishing_event.domain.core.UserFishingEventEntity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserFishingEventRepository {
+
+    boolean existsByUser_IdAndFishingEvent_Id(
+            UUID userId,
+            UUID fishingEventId
+    );
+
+    Optional<UserFishingEventEntity> findByUser_IdAndFishingEvent_Id(
+            UUID userId,
+            UUID fishingEventId
+    );
+
+    List<UserFishingEventEntity> findByUser_Id(UUID userId);
+
+    List<UserFishingEventEntity> findByFishingEvent_Id(UUID fishingEventId);
+
+    long countByFishingEvent_Id(UUID fishingEventId);
+}
---
+++ src/main/java/smartfish/modules/user_fishing_event/infrastruture/repository/impl/UserFishingEventRepositoryImpl.java
@@ -0,0 +1,50 @@
+package smartfish.modules.user_fishing_event.infrastruture.repository.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import smartfish.modules.user_fishing_event.domain.core.UserFishingEventEntity;
+import smartfish.modules.user_fishing_event.domain.repository.UserFishingEventRepository;
+import smartfish.modules.user_fishing_event.infrastruture.repository.jpa.IUserFishingEventJpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class UserFishingEventRepositoryImpl implements UserFishingEventRepository {
+
+    private final IUserFishingEventJpaRepository repository;
+
+    @Override
+    public boolean existsByUser_IdAndFishingEvent_Id(UUID userId,
+                                                     UUID fishingEventId) {
+        return repository.existsByUser_IdAndFishingEvent_Id(
+                userId,
+                fishingEventId);
+    }
+
+    @Override
+    public Optional<UserFishingEventEntity> findByUser_IdAndFishingEvent_Id(
+            UUID userId,
+            UUID fishingEventId) {
+        return repository.findByUser_IdAndFishingEvent_Id(
+                userId,
+                fishingEventId);
+    }
+
+    @Override
+    public List<UserFishingEventEntity> findByUser_Id(UUID userId) {
+        return repository.findByUser_Id(userId);
+    }
+
+    @Override
+    public List<UserFishingEventEntity> findByFishingEvent_Id(UUID fishingEventId) {
+        return repository.findByFishingEvent_Id(fishingEventId);
+    }
+
+    @Override
+    public long countByFishingEvent_Id(UUID fishingEventId) {
+        return repository.countByFishingEvent_Id(fishingEventId);
+    }
+}
