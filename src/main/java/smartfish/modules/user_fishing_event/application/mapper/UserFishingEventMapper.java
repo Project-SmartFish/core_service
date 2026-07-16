f24c0c760f26e69761abb5d0063330d837fcc87a
GustavoHPelissari <gustavo.h.pelissari@gmail.com>
2026-07-16 17:56:22 -0300

    feat(UserFishingEvent): adiciona mapper de UserFishingEvent

---
+++ src/main/java/smartfish/modules/user_fishing_event/application/mapper/UserFishingEventMapper.java
@@ -0,0 +1,32 @@
+package smartfish.modules.user_fishing_event.application.mapper;
+
+import org.springframework.stereotype.Component;
+import smartfish.modules.fishing_event.domain.core.FishingEventEntity;
+import smartfish.modules.user.domain.entity.UserEntity;
+import smartfish.modules.user_fishing_event.application.dto.request.join.JoinFishingEventRequest;
+import smartfish.modules.user_fishing_event.application.dto.response.join.JoinFishingEventResponse;
+import smartfish.modules.user_fishing_event.domain.core.UserFishingEventEntity;
+
+@Component
+public class UserFishingEventMapper {
+
+    public UserFishingEventEntity toEntity(JoinFishingEventRequest request) {
+        FishingEventEntity fishingEvent = FishingEventEntity
+                .builder()
+                .id(request.fishingEventId())
+                .build();
+
+        return UserFishingEventEntity
+                .builder()
+                .fishingEvent(fishingEvent)
+                .build();
+    }
+
+    public JoinFishingEventResponse toJoinResponse(UserFishingEventEntity entity) {
+        return new JoinFishingEventResponse(
+                entity.getId(),
+                entity.getUser().getId(),
+                entity.getFishingEvent().getId()
+        );
+    }
+}
