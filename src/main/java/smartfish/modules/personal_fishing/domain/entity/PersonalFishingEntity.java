c90c0e0fef884bfbca627f5dc87a8d8eb4e0f997
GustavoHPelissari <gustavo.h.pelissari@gmail.com>
2026-07-16 17:14:02 -0300

    feat: adiciona relacionamento entre PersonalFishing e UserFishingEvent

--- src/main/java/smartfish/modules/personal_fishing/domain/entity/PersonalFishingEntity.java
+++ src/main/java/smartfish/modules/personal_fishing/domain/entity/PersonalFishingEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import smartfish.modules.user_fishing_event.domain.core.UserFishingEventEntity;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -19,6 +20,14 @@ public class PersonalFishingEntity {
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id")
     private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+            name = "user_fishing_event_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_personal_fishing_user_fishing_event")
+    )
+    private UserFishingEventEntity userFishingEvent;
 
     @Column(nullable = false, name = "specie")
     private String specie;
